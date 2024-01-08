6bb3ec4abcb2b25d3f888dbb3d503ac9472c82b9
JoeMad21 <joeymadden21@gmail.com>
2024-01-08 07:50:59 -0500

    Updated hpp to have correct argument

--- device_libraries/firehose_ipu.hpp
+++ device_libraries/firehose_ipu.hpp
@@ -11,7 +11,7 @@ void printMatrix(std::string matrix_name, std::vector<float> matrix, int cols);
 
 void frontEnd_TensorDecomp(bool& flag, int& rows, int& cols, int& exp_size, std::vector<float>& cpu_input0, std::vector<float>& cpu_output0, std::vector<float>& cpu_output1);
 
-void backEnd_TensorDecomp(poplar::Engine engine, bool& flag, int& exp_size) {
+void backEnd_TensorDecomp(poplar::Engine& engine, bool& flag, int& exp_size) {
 
     for (int i = 0; i < exp_size; i++) {
         while(!flag) {}
