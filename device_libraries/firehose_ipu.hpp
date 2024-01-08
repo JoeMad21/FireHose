9e6599547ee9b52ae794fc457e88f52cf2e34848
JoeMad21 <joeymadden21@gmail.com>
2024-01-08 08:17:44 -0500

    fixed hpp error

--- device_libraries/firehose_ipu.hpp
+++ device_libraries/firehose_ipu.hpp
@@ -19,15 +19,6 @@ void printMatrix(std::string matrix_name, std::vector<float> matrix, int cols);
 
 void frontEnd_TensorDecomp(bool& flag, long unsigned int& rows, long unsigned int& cols, long unsigned int& exp_size, std::vector<float>& cpu_input0, std::vector<float>& cpu_output0, std::vector<float>& cpu_output1);
 
-void backEnd_TensorDecomp(poplar::Engine& engine, bool& flag, long unsigned int& exp_size) {
+void backEnd_TensorDecomp(poplar::Engine& engine, bool& flag, long unsigned int& exp_size);
 
-    for (int i = 0; i < exp_size; i++) {
-        while(!flag) {}
-        flag = false;
-        engine.run(STREAM_INPUTS);
-        engine.run(CONSUMPTION_TASK);
-        engine.run(STREAM_RESULTS);
-    }
-}
-
-void tensorDecomp() 
+void tensorDecomp();
