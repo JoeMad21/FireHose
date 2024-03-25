c9138e98c09cbde92a8ea5d2395dae90e345011f
JoeMad21 <joeymadden21@gmail.com>
2024-03-24 23:16:03 -0400

    Added missing colon

--- device_libraries/firehose_ipu.cpp
+++ device_libraries/firehose_ipu.cpp
@@ -103,7 +103,7 @@ void tensorDecomp(long unsigned int row, long unsigned int col, long unsigned in
         //v_con1[i] = graph.addVariable(poplar::FLOAT, {row, col}, db_name);
         //poputil::mapTensorLinearly(graph, v_con1[i]);
 
-        db_name = "Output Tensor " + std::to_string(i) + " of Set 0"
+        db_name = "Output Tensor " + std::to_string(i) + " of Set 0";
         v_io_out0[i] = graph.addVariable(poplar::FLOAT, {row, col}, db_name);
         poputil::mapTensorLinearly(graph, v_io_out0[i]);
 
