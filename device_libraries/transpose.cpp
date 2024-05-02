cfef1bfb207ed424596943a8579e76e335ed7ee7
JoeMad21 <joeymadden21@gmail.com>
2024-05-02 10:26:26 -0400

    Somehow got the same matrix back

--- device_libraries/transpose.cpp
+++ device_libraries/transpose.cpp
@@ -12,7 +12,7 @@ Vector<Output<Vector<float>>> strm_out;
     bool compute() {
         for (unsigned i = 0; i < strm_out.size(); i++) {
             for (unsigned j = 0; j < strm_out.size(); j++) {
-                strm_out[i][j] = strm_in[j][i];
+                strm_out[j][i] = strm_in[i][j];
             }
         }
         return true;
