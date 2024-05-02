1e1f2f7d9a5c466e246eed211fbbb8ddd66413df
JoeMad21 <joeymadden21@gmail.com>
2024-05-02 06:13:25 -0400

    Forgot to include in header

--- device_libraries/firehose_ipu.hpp
+++ device_libraries/firehose_ipu.hpp
@@ -42,15 +42,15 @@ struct comPatternTriangleUP {
     } cps;
 
     struct {
-        std::vector<poplar::VertexRef> in0(num_streams);
-        std::vector<poplar::VertexRef> out0(num_streams);
-        std::vector<poplar::VertexRef> out1(num_streams);
+        std::vector<poplar::VertexRef> in0;
+        std::vector<poplar::VertexRef> out0;
+        std::vector<poplar::VertexRef> out1;
     } vtx;
 
     struct {
-        std::vector<poplar::DataStream> in0(num_streams);
-        std::vector<poplar::DataStream> out0(num_streams);
-        std::vector<poplar::DataStream> out1(num_streams);
+        std::vector<poplar::DataStream> in0;
+        std::vector<poplar::DataStream> out0;
+        std::vector<poplar::DataStream> out1;
     } strm;
 }
 
@@ -62,6 +62,20 @@ void createIdentityMatrix(std::vector<float>& vec_id, int row, int col);
 poplar::Device getDevice(int hw_mode, int num_devices);
 
 void buildLayer(poplar::Graph& graph, model& myModel, std::pair<int,int> params, int layer_id, int map, int num_tensors);
+
+void addComputeSet(poplar::Graph& graph, std::vector<poplar::ComputeSet>& cps, int num_streams, int IO);
+
+void addStream(poplar::Graph& graph, std::vector<poplar::DataStream>& strm, std::pair<int,int> params, int buf_depth, int num_port, int num_streams, int IO);
+
+void addVertex(poplar::Graph& graph, std::vector<poplar::VertexRef>& vtx, int offset);
+
+void connectVertex(poplar::Graph& graph, std::vector<poplar::VertexRef>& vtx, std::vector<model>& myModels, int top_layer, int bottom_layer, int top_tensor, int bottom_tensor, std::string in, std::string out);
+
+void connectEngineStream(poplar::Graph& graph, std::vector<float>& cpu, int num_streams, int num_port, int IO);
+
+void buildTensorTemplateTRIANGLEUP(poplar::Graph& graph, std::vector<model>& myModels, std::pair<int,int> params, int num_streams);
+
+void buildIOTemplateTRIANGLEUP(poplar::Graph& graph, std::vector<model>& myModels, comPatternTriangleUP& comPat, std::pair<int,int> params, int num_streams);
 
 void tensorDecomp(long unsigned int row, long unsigned int col, long unsigned int num_packets, long unsigned int num_streams, long unsigned int num_devices, long unsigned int seed, bool get_from_file);
 
