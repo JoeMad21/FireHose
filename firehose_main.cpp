eef3a932f5c1b1ae4c9b04836a4da9b31776c49f
JoeMad21 <joeymadden21@gmail.com>
2024-05-09 07:06:44 -0400

    Access still not correct

--- firehose_main.cpp
+++ firehose_main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
             return 1;
         }
 
-    switch(vm["con_task"]) {
+    switch(vm["con_task"].as<int>()) {
         case TASK::TENSOR_DECOMP:
             tensorDecomp(row, col, num_packets, num_streams, num_devices, seed, get_from_file);
             break;
