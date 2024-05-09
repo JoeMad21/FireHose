5ac6de427e26214fefa4bbeda879be3c7118e78e
JoeMad21 <joeymadden21@gmail.com>
2024-05-09 08:30:13 -0400

    Trying to output multiple reports

--- test.sh
+++ test.sh
@@ -2,11 +2,17 @@
 
 echo "STARTING"
 
-for value in {1..3}
+export POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "autoReport.directory":"./report"}'
+
+for task in {1..3}
 do
-    sbatch demo.batch
-    sleep 2m
-    make refresh
+    for dim in {2, 4, 8}
+    do
+        export POPLAR_ENGINE_OPTIONS='{"autoReport.directory":"./report_${task}_${dim}"}'
+        sbatch demo.batch
+        sleep 5m
+        make refresh
+    done
 done
 
 echo "DONE"
