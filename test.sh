4cd6f30d2ca2f2a772188b41fd66c1e770433773
JoeMad21 <joeymadden21@gmail.com>
2024-05-23 07:01:55 -0400

    Mistake in variable usage

--- test.sh
+++ test.sh
@@ -4,13 +4,13 @@ echo "STARTING"
 
 export POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "autoReport.directory":"./report"}'
 
-$example = '{"autoReport.directory":"./fout"}'
+example = '{"autoReport.directory":"./fout"}'
 
 for task in {1..3}
 do
     for dim in {2, 4, 8}
     do
-        export POPLAR_ENGINE_OPTIONS=${example}
+        export POPLAR_ENGINE_OPTIONS=$example
         sleep 2s
         sbatch demo.batch
         sleep 5m
