06990a1393a7534219f0166af678859c7578ff0f
JoeMad21 <joeymadden21@gmail.com>
2024-05-23 07:16:44 -0400

    Getting some help from GPT

--- test.sh
+++ test.sh
@@ -2,18 +2,23 @@
 
 echo "STARTING"
 
-export POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "autoReport.directory":"./report"}'
-
-example='{"autoReport.directory":"./fout"}'
-
 for task in {1..3}
 do
     for dim in {2, 4, 8}
     do
-        export POPLAR_ENGINE_OPTIONS=$example
+        # Set the directory dynamically based on task and dim
+        export POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "autoReport.directory":"./report_task'${task}'_dim'${dim}'"}'
+        echo "Running task ${task} with dimension ${dim}"
+        
         sleep 2s
+        
+        # Submit the job
         sbatch demo.batch
+        
+        # Wait for the job to complete
         sleep 5m
+        
+        # Refresh the environment or application
         make refresh
     done
 done
