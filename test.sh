1def3f717d2bebde163bea7e1b7c553608115313
JoeMad21 <joeymadden21@gmail.com>
2024-05-23 06:57:43 -0400

    Trying a variable

--- test.sh
+++ test.sh
@@ -4,11 +4,14 @@ echo "STARTING"
 
 export POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "autoReport.directory":"./report"}'
 
+$example = '{"autoReport.directory":"./fout"}'
+
 for task in {1..3}
 do
     for dim in {2, 4, 8}
     do
-        #export POPLAR_ENGINE_OPTIONS='{"autoReport.directory":"./fout_${task}"}'
+        export POPLAR_ENGINE_OPTIONS=$example
+        sleep 2s
         sbatch demo.batch
         sleep 5m
         make refresh
