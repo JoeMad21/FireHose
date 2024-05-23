6d2e179bd2fa1755af54f5459619a9a6dc3eed34
JoeMad21 <joeymadden21@gmail.com>
2024-05-23 07:53:25 -0400

    Passing arguments

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ echo "STARTING"
 
 for task in {1..3}
 do
-    for dim in 2, 4, 8
+    for dim in 2 4 8
     do
         # Set the directory dynamically based on task and dim
         export POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "autoReport.directory":"./report_task'${task}'_dim'${dim}'"}'
@@ -13,7 +13,7 @@ do
         sleep 2s
         
         # Submit the job
-        sbatch demo.batch
+        sbatch demo.batch ${task}
         
         # Wait for the job to complete
         sleep 5m
