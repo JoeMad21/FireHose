3d4f347f994a9253961bf7a5202f86f1f216f6a5
JoeMad21 <joeymadden21@gmail.com>
2024-05-23 07:00:28 -0400

    Using brackets

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ for task in {1..3}
 do
     for dim in {2, 4, 8}
     do
-        export POPLAR_ENGINE_OPTIONS=$example
+        export POPLAR_ENGINE_OPTIONS=${example}
         sleep 2s
         sbatch demo.batch
         sleep 5m
