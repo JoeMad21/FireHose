4cff927776c31471792a009ff7c3ba37004ad037
JoeMad21 <joeymadden21@gmail.com>
2024-05-26 03:44:14 -0400

    Automating several tests

--- test.sh
+++ test.sh
@@ -2,24 +2,27 @@
 
 echo "STARTING"
 
-for task in {1..3}
+for run in {0..3}
 do
-    for dim in 2 4 8
+    for task in {0..3}
     do
-        # Set the directory dynamically based on task and dim
-        export POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "autoReport.directory":"./reports/report_task'${task}'_dim'${dim}'"}'
-        echo "Running task ${task} with dimension ${dim}"
+        for dim in 2 4 8 16 32 64 128 256
+        do
+            # Set the directory dynamically based on task and dim
+            export POPLAR_ENGINE_OPTIONS='{"autoReport.all":"true", "autoReport.directory":"./reports/report_run'${run}'_task'${task}'_dim'${dim}'"}'
+            echo "Running task ${task} with dimension ${dim}"
         
-        sleep 2s
+            sleep 2s
         
-        # Submit the job
-        sbatch demo.batch ${task}
+            # Submit the job
+            sbatch demo.batch ${task}
         
-        # Wait for the job to complete
-        sleep 5m
+            # Wait for the job to complete
+            sleep 5m
         
-        # Refresh the environment or application
-        make refresh
+            # Refresh the environment or application
+            make refresh
+        done
     done
 done
 
